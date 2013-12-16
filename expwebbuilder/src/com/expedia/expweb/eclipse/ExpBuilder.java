883aa4f9968385e38cb03c2ab0b8b8518e981fe5
chentong <chentongtc@gmail.com>
2013-12-16 18:24:28 +0000

    working version

--- expwebbuilder/src/com/expedia/expweb/eclipse/ExpBuilder.java
+++ expwebbuilder/src/com/expedia/expweb/eclipse/ExpBuilder.java
@@ -1,13 +1,18 @@
 package com.expedia.expweb.eclipse;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.core.resources.ProjectScope;
+import org.osgi.service.log.LogService;
 import org.osgi.service.prefs.Preferences;
 import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProjectDescription;
@@ -15,11 +20,16 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.IWorkspaceDescription;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.springsource.ide.eclipse.gradle.core.actions.RefreshAllActionCore;
 
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import org.osgi.service.prefs.BackingStoreException;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
@@ -27,51 +37,106 @@ import org.eclipse.core.runtime.preferences.InstanceScope;
 
 public class ExpBuilder implements IWorkbenchWindowActionDelegate {
 	private IWorkbenchWindow window = null;
+	@Inject
+	private LogService log;
+
+	public ExpBuilder() {
+
+	}
+	
+	private void openMsgBox(String msg) {
+		MessageBox box = new MessageBox(window.getShell(),
+				SWT.ICON_INFORMATION);
+		InputDialog dialog = new InputDialog(window.getShell(),
+				"Lets try!", msg, "", null);
+		dialog.open();
+	}
 
 	@Override
 	public void run(IAction action) {
-		IWorkspace ws = ResourcesPlugin.getWorkspace();
-		IWorkspaceDescription desc = ws.getDescription();
-		boolean autoBuildConf = desc.isAutoBuilding();
-		desc.setAutoBuilding(false);
-		try {
-			ws.setDescription(desc);
-		} catch (CoreException ex) {
-			ex.printStackTrace();
-		}
+		//Job job = new Job("Test Job") {
+		//	@Override
+		//	protected IStatus run(IProgressMonitor monitor) {
+		//		// Set total number of work units
+		//		monitor.beginTask("start task", 100);
 
-		// https://github.com/spring-projects/eclipse-integration-gradle/blob/master/org.springsource.ide.eclipse.gradle.ui/src/org/springsource/ide/eclipse/gradle/ui/actions/RefreshAllAction.java
-		try {
-			RefreshAllActionCore.callOn(Arrays.asList(ws.getRoot()
-					.getProjects()));
-		} catch (CoreException ex) {
-			ex.printStackTrace();
-		}
+				final IWorkspace ws = ResourcesPlugin.getWorkspace();
+				IWorkspaceDescription desc = ws.getDescription();
+				boolean autoBuildConf = desc.isAutoBuilding();
+				desc.setAutoBuilding(false);
+				try {
+					ws.setDescription(desc);
+					//openMsgBox("desc set");
+				} catch (CoreException ex) {
+					ex.printStackTrace();
+				}
 
-		try {
-			cleanAndBuild(ws, "platform", false);
-			cleanAndBuild(ws, "dataaccess", false);
-			cleanAndBuild(ws, "domain", true);
-			cleanAndBuild(ws, "webdomain-api", false);
-			cleanAndBuild(ws, "webdomain", false);
-			cleanAndBuild(ws, "checkout.ui", true);
-			cleanAndBuild(ws, "stub", false);
-		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		desc.setAutoBuilding(autoBuildConf);
-		try {
-			ws.setDescription(desc);
-		} catch (CoreException ex) {
-			ex.printStackTrace();
-		}
+				// log.log(LogService.LOG_WARNING, "starting gradle refresh");
+				// https://github.com/spring-projects/eclipse-integration-gradle/blob/master/org.springsource.ide.eclipse.gradle.ui/src/org/springsource/ide/eclipse/gradle/ui/actions/RefreshAllAction.java
+					openMsgBox("Starting gradle");
+					try {
+						RefreshAllActionCore.callOn(new ArrayList<IProject>(){{
+							//openMsgBox("gradle start ");
+							add(ws.getRoot().getProject("platform"));
+							add(ws.getRoot().getProject("dataaccess"));
+							add(ws.getRoot().getProject("domain"));
+							add(ws.getRoot().getProject("webdomain-api"));
+							add(ws.getRoot().getProject("webdomain"));
+							add(ws.getRoot().getProject("shared.ui"));
+							add(ws.getRoot().getProject("checkout.ui"));
+							add(ws.getRoot().getProject("stub"));
+							add(ws.getRoot().getProject("integration.test"));
+							//openMsgBox("gradle stop ");
+						}}).join();
+					} catch (CoreException e1) {
+						openMsgBox("clean build error " + e1.getMessage());
+						e1.printStackTrace();
+					}
+					//RefreshAllActionCore.callOn(Arrays.asList(ws.getRoot().getProjects()));
+				// log.log(LogService.LOG_WARNING, "completed gradle refresh");
+					catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 
-		new ScopedPreferenceStore(InstanceScope.INSTANCE,
-				"bundle-name-of-other-plugin");
+				try {
+					//openMsgBox("starting platform clean build");
+					cleanAndBuild(ws, "platform", false);
+					//openMsgBox("starting dataaccess clean build");
+					cleanAndBuild(ws, "dataaccess", false);
+					//openMsgBox("starting domain clean build");
+					cleanAndBuild(ws, "domain", true);
+					//openMsgBox("starting webdomain api clean build");
+					cleanAndBuild(ws, "webdomain-api", false);
+					//openMsgBox("starting webdomain clean build");
+					cleanAndBuild(ws, "webdomain", false);
+					//openMsgBox("starting shared.ui clean build");
+					cleanAndBuild(ws, "shared.ui", false);
+					//openMsgBox("starting checkout.ui clean build");
+					cleanAndBuild(ws, "checkout.ui", true);
+					//openMsgBox("starting stub clean build");
+					cleanAndBuild(ws, "stub", false);
+				} catch (BackingStoreException e) {
+					openMsgBox("clean build error " + e.getMessage());
+					e.printStackTrace();
+				} catch (CoreException e) {
+					openMsgBox("clean build error " + e.getMessage());
+					e.printStackTrace();
+				}
+				desc.setAutoBuilding(autoBuildConf);
+				try {
+					//openMsgBox("set desc back");
+					ws.setDescription(desc);
+				} catch (CoreException ex) {
+					ex.printStackTrace();
+				}
+				
+				openMsgBox("expweb build complete");
+		//		return Status.OK_STATUS;
+		//	}
+		//};
+		//job.setRule(ResourcesPlugin.getWorkspace().getRoot());
+		//job.schedule();
 	}
 
 	private void cleanAndBuild(IWorkspace ws, String projectName,
